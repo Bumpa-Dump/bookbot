8a10dfa7de28dd59b65d28557a4b48a656971e8b
Bumpa-Dump <jacobpolovina2@gmail.com>
2024-06-04 17:30:34 -0500

    Finalized main.py

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 def main(location):
     with open(location) as f:
-        file_contents = f.read()
+        file_contents = f.read().lower()
     usable_dict = charcount(file_contents)
+    usable_dict.sort(key=organize, reverse=True)
     print("--- Begin Report of ", location, " ---")
     print("Total Word Count: ", wordcount(file_contents))
     print("")
-    for entry in usable_dict:
-        print("The '", entry, "' character appeared ", usable_dict[entry], " times.")
+    presentation(usable_dict)
 
 def wordcount(text):
     wordslist = text.split()
     return len(wordslist)
 
 def charcount (text):
-    char_dict = {"a" : 0}
+    char_dict = {}
+    output_list = list()
     for char in text:
-        if char in char_dict:
+        if char.isalpha() == False:
+            pass
+        elif char in char_dict:
             char_dict[char] += 1
         else:
             char_dict[char] = 1
-    return(char_dict)
+    for item in char_dict:
+        output_list.append({"letter": item, "count": char_dict[item]})
+    return output_list
+
+def organize(dict):
+    return dict["count"]
+
+def presentation(ls_of_dicts):
+    for item in ls_of_dicts:
+        print("The ", item["letter"], " character was found ", item["count"], " times")
 
 
 main("books/frankenstein.txt")
-
-# need to convert dictionary to list of dictionaries (list.append (temp_dict establish values?)), remove non-alphabetic characters (boring), and organize by frequency
